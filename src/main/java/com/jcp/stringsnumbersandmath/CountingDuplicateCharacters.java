b9eb02531db63a22e26a983bfdb191f24747678c
Aakash <aakashsrh@gmail.com>
2025-03-03 22:13:19 +0530

    added CountingDuplicateCharacters and readme.md

--- src/main/java/com/jcp/stringsnumbersandmath/CountingDuplicateCharacters.java
+++ src/main/java/com/jcp/stringsnumbersandmath/CountingDuplicateCharacters.java
@@ -1,5 +1,25 @@
 package com.jcp.stringsnumbersandmath;
 
-public class CountingDuplicateCharacters  {
+import java.util.HashMap;
+import java.util.Map;
 
+public class CountingDuplicateCharacters  {
+    // Count the duplicate characters in a given String
+    public static void main(String[] args) {
+        // For Example there is a String = "aakash sirohi";
+        // Return or Print the number of Duplicate Characters in this String
+
+        HashMap<Character,Integer> characterHashMap = new HashMap<>();
+        String s = "aakash sirohi";
+        for(int i = 0;i<s.length();i++){
+            if(!characterHashMap.containsKey(s.charAt(i))){
+                characterHashMap.put(s.charAt(i),1);
+            }else{
+                characterHashMap.put(s.charAt(i),characterHashMap.get(s.charAt(i))+1);
+            }
+        }
+        for(Map.Entry<Character,Integer> p : characterHashMap.entrySet()){
+            System.out.println(p.getKey() + " " + p.getValue());
+        }
+    }
 }
